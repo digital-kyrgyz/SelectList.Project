df7c55511034d3baa99d7307bf41f8afd30bf0ac
melis-archabaev <melis99mrx@gmail.com>
2022-04-18 12:22:03 +0600

    List Json Created

---
+++ SelectList.Project/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SelectList.Project.Models;
+using SelectList.Project.ViewModels;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SelectList.Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var jsonList = _configuration.GetSection("Needs:People").Get<List<PeopleVm>>().ToList();
+            ViewData["People"] = jsonList;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SelectList.Project/ViewModels/PeopleVm.cs
@@ -0,0 +1,11 @@
+﻿namespace SelectList.Project.ViewModels
+{
+    public class PeopleVm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Number { get; set; }
+    }
+}
